e0f346537a782d82dbc6616d55f50cd579d8eb50
HenshinIT <98543268+HenshinIT@users.noreply.github.com>
2022-01-29 14:18:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     introDesc: `Anh thích em , em làm người iuuu anh nhá . Anh không dám hứa sẽ cho em hạnh phúc . 
     Nhưng anh chắc chắn 1 điều không ai mang cho em hạnh phúc bằng anh <3 `,
     btnIntro: 'Nhấn tiếp tục để đáp lại lời tỏ tình này :3',
-    title: 'Anh iuuu em làm người iuuu anh nhé babe <3 ',
+    title: 'Anh iuuu em , làm người iuuu anh nhé babe <3 ',
     desc: 'Em có 2 lựa chọn hãy chọn theo ý muốn của em :3 ',
     btnYes: 'I love U so much <33',
     btnNo: 'No , I do not love u :3',
