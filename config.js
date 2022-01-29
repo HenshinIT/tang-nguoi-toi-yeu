fc45c923c0b671fdb0a17c87a0ea865479b473fd
Tên của bạn <you@example.com>
2022-01-29 14:09:13 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Anh thích em , em làm người iuuu anh nhá . Anh không dám hứa sẽ cho em hạnh phúc . 
+    Nhưng anh chắc chắn 1 điều không ai mang cho em hạnh phúc bằng anh <3 `,
+    btnIntro: 'Nhấn tiếp tục để đáp lại lời tỏ tình này :3',
+    title: 'Anh iuuu em làm người iuuu anh nhé babe <3 ',
+    desc: 'Em có 2 lựa chọn hãy chọn theo ý muốn của em :3 ',
+    btnYes: 'I love U so much <33',
+    btnNo: 'No , I do not love u :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Cho anh cái hẹn 2 đứa mình đi chơi nè babe :333.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/cuongquocit/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
