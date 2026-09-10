6fecadf221808ee69fccee91e6465456f2e00893
HenshinIT <98543268+HenshinIT@users.noreply.github.com>
2026-09-10 22:33:57 +0700

    Update CONFIG messages and questions in config.js

--- config.js
+++ config.js
@@ -2,17 +2,17 @@
 const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Anh thích em , em làm người iuuu anh nhá . Anh không dám hứa sẽ cho em hạnh phúc . 
-    Nhưng anh chắc chắn 1 điều không ai mang cho em hạnh phúc bằng anh <3 `,
+    Nhưng anh chắc chắn 1 điều không ai mang cho em cảm giác an toàn và hạnh phúc bằng anh <3 `,
     btnIntro: 'Nhấn tiếp tục để đáp lại lời tỏ tình này :3',
     title: 'Anh iuuu em , làm người iuuu anh nhé babe <3 ',
     desc: 'Em có 2 lựa chọn hãy chọn theo ý muốn của em :3 ',
     btnYes: 'I love U so much <33',
     btnNo: 'No , I do not love u :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại chọn đồng hành cùng anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối thứ 7 lúc 18h nha babe :333.',
+    reply: 'Em đồng ý làm người yêu anh <33333333',
+    mess: 'Anh biết mà 🥰. Anh cũng thích em lắm á 😘😘',
+    messDesc: 'Tối thứ 6 ngày mai lúc 18h nha babe :333.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/itgamo2002' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
